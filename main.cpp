25dc43a61459d80e8aaecc5ed6ac2d25c2e7871d
BindyukD <bindiuckk@mail.ru>
2018-06-06 02:00:59 +0300

    a/b

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main()
  cout << "Enter A and B: ";
  cin >> a >> b;
  cout << "A + B = " << a + b << '\n';
-  cout << "A * 2 = " << a * 2 << '\n';
+  cout << "A / 2 = " << a / 2 << '\n';
   cout << "A - B = " << a - b << '\n';
  }
